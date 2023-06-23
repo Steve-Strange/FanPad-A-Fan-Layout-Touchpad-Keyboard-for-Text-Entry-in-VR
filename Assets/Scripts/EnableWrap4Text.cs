ae49c8f96fc567d6324065ffa7c3b167c88dec5f
Namisntimpot <1346865561@qq.com>
2023-06-23 12:35:02 +0800

    scripts recovery

---
+++ Assets/Scripts/EnableWrap4Text.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EnableWrap4Text : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextMeshProUGUI text = GetComponent<TextMeshProUGUI>();
+        text.enableWordWrapping = true;
+        text.overflowMode = TextOverflowModes.ScrollRect;
+    }
+    /* TODO: ʹ�� Pageģʽ��������д���ʱ���Զ���ҳ!
+     * ��Ӧ����Ҫ����InputField��һЩ״̬����ʵ�ֻص�����(�������Ӧ���¼��Ļ�).
+     */
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
