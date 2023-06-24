bdf3d7ff6d4925894e893a7ebd1339903e441a23
Namisntimpot <1346865561@qq.com>
2023-06-24 12:29:45 +0800

    inclinedKeyboard-Axis2Letter 2

--- Assets/Scripts/InclinedKeyboard.cs
+++ Assets/Scripts/InclinedKeyboard.cs
@@ -6,9 +6,9 @@ using Valve.VR;
 /* 倾斜键盘，继承自ClickKeyboard，应当只用实现自己的Axis2Letter方法. */
 public class InclinedKeyboard : ClickKeyboard
 {
-    public float theta;
-    public float thumbLength;
     private float radius = 1;
+    public float thumbTheta;
+    public float thumbLength;
     private Vector2[] thumbCenter = new Vector2[7];
     private float[] d = new float[7];
 
@@ -21,7 +21,7 @@ public class InclinedKeyboard : ClickKeyboard
         for (int i = 0; i < 7; i++)
         {
             d[i] = thumbLength + (i - 3) * 1 / 3;
-            thumbCenter[i] = new Vector2((d[i] * Mathf.Sin(theta)), (d[i] * Mathf.Cos(theta)));
+            thumbCenter[i] = new Vector2((d[i] * Mathf.Sin(thumbTheta)), (d[i] * Mathf.Cos(thumbTheta)));
         }
     }
 
@@ -35,13 +35,20 @@ public class InclinedKeyboard : ClickKeyboard
             float distanceB = Mathf.Sqrt(Mathf.Pow((axis.x - thumbCenter[i+1].x), 2) + Mathf.Pow((axis.y - thumbCenter[i+1].y), 2));
             if (distanceA > thumbLength && distanceB < thumbLength)
             {
-                column = i + 1;
+                column = i;
                 break;
             }
         }
 
-        float thetaMax = Mathf.Acos((Mathf.Pow(d[column], 2) + Mathf.Pow(thumbLength, 2) - Mathf.Pow(radius, 2)) / 2 * d[column] * thumbLength);
+        float thetaMax = Mathf.Acos((Mathf.Pow(d[column + 1], 2) + Mathf.Pow(thumbLength, 2) - Mathf.Pow(radius, 2)) / 2 * d[column + 1] * thumbLength);
 
-        return 0;
+        float currentTheta = Mathf.Abs(Mathf.Atan((axis.x - thumbCenter[column + 1].x) / (axis.y - thumbCenter[column + 1].y))) - thumbTheta;
+
+        float frow = (currentTheta + thetaMax) / (2 * thetaMax / keyColumn[column]);
+
+        row = (int)frow;
+
+
+        return keys[column, row, mode];
     }
 }
